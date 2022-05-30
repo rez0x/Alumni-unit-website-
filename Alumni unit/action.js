d6de35c8b953644425d6fc6171168bdf9a40b1a8
rez0x <100443543+rez0x@users.noreply.github.com>
2022-05-30 07:38:07 +0300

    Add files via upload

---
+++ Alumni unit/action.js
@@ -0,0 +1,459 @@
+var box = [document.getElementById("manger_word"), document.getElementById("about_unit"),document.getElementById("implementation_mechanisms"),document.getElementById("organizational_structure"),document.getElementById("unit_members"),document.getElementById("alumni_stats"),document.getElementById("alumni_honor_board"),document.getElementById("alumni_magazine"),document.getElementById("important_topics") , document.getElementById("unit_models") ,  document.getElementById("photo_album"), document.getElementById("connect_us"),document.getElementById("high_achievers"),document.getElementById("workshop"),document.getElementById("the_line"),document.getElementById("news_ticker")];
+var box1 = document.getElementById("manger_word");
+var box2 = document.getElementById("about_unit");
+var box3 = document.getElementById("implementation_mechanisms");
+var box4 = document.getElementById("organizational_structure");
+var box5 = document.getElementById("unit_members");
+var box6 = document.getElementById("alumni_stats");
+var box7 = document.getElementById("alumni_honor_board");
+var box8 = document.getElementById("alumni_magazine");
+var box9 = document.getElementById("important_topics");
+var box10 = document.getElementById("unit_models");
+var box11 = document.getElementById("photo_album");
+var box12 = document.getElementById("connect_us");
+var box13 = document.getElementById("high_achievers");
+var box14 = document.getElementById("workshop");
+var box15 = document.getElementById("the_line");
+var box16 = document.getElementById("news_ticker");
+
+
+
+
+
+document.getElementById("co_us").addEventListener("click", function(){
+
+
+   if(box12.style.display=="none")
+   {  var n = 0;
+      while(n < 16)
+      {  
+         box[n].style.display = "none";
+         n++;
+      }
+      box12.style.display="block";
+   }
+
+   else 
+   {
+      box12.style.display="none";
+
+
+
+
+   }
+
+})
+
+
+document.getElementById("rb1").addEventListener("click", function(){
+
+
+   if(box1.style.display=="none")
+   {  var n = 0;
+      while(n < 16)
+      {  
+         box[n].style.display = "none";
+         n++;
+      }
+      box1.style.display="block";
+   }
+
+   else 
+   {
+      box1.style.display="none";
+
+
+
+
+   }
+
+})
+
+
+document.getElementById("rb2").addEventListener("click", function(){
+
+
+   if(box2.style.display=="none")
+   {  var n = 0;
+      while(n < 16)
+      {  
+         box[n].style.display = "none";
+         n++;
+      }
+      box2.style.display="block";
+   }
+
+   else 
+   {
+      box2.style.display="none";
+
+
+
+
+   }
+
+})
+document.getElementById("rb3").addEventListener("click", function(){
+
+
+   if(box3.style.display=="none")
+   {  var n = 0;
+      while(n < 16)
+      {  
+         box[n].style.display = "none";
+         n++;
+      }
+      box3.style.display="block";
+   }
+
+   else 
+   {
+      box3.style.display="none";
+
+
+
+
+   }
+
+}
+)
+document.getElementById("rb4").addEventListener("click", function(){
+
+
+   if(box4.style.display=="none")
+   {  var n = 0;
+      while(n < 16)
+      {  
+         box[n].style.display = "none";
+         n++;
+      }
+      box4.style.display="block";
+   }
+
+   else 
+   {
+      box4.style.display="none";
+
+
+
+
+   }
+
+})
+document.getElementById("rb5").addEventListener("click", function(){
+
+
+   if(box5.style.display=="none")
+   {  var n = 0;
+      while(n < 16)
+      {  
+         box[n].style.display = "none";
+         n++;
+      }
+      box5.style.display="block";
+   }
+
+   else 
+   {
+      box5.style.display="none";
+
+
+
+
+   }
+
+})
+document.getElementById("rb6").addEventListener("click", function(){
+
+
+   if(box6.style.display=="none")
+   {  var n = 0;
+      while(n < 16)
+      {  
+         box[n].style.display = "none";
+         n++;
+      }
+      box6.style.display="block";
+   }
+
+   else 
+   {
+      box6.style.display="none";
+
+
+
+
+   }
+
+})
+document.getElementById("rb7").addEventListener("click", function(){
+
+
+   if(box7.style.display=="none")
+   {  var n = 0;
+      while(n < 16)
+      {  
+         box[n].style.display = "none";
+         n++;
+      }
+      box7.style.display="block";
+   }
+
+   else 
+   {
+      box7.style.display="none";
+
+
+
+
+   }
+
+})
+
+
+document.getElementById("rb8").addEventListener("click", function(){
+
+
+   if(box8.style.display=="none")
+   {  var n = 0;
+      while(n < 16)
+      {  
+         box[n].style.display = "none";
+         n++;
+      }
+      box8.style.display="block";
+   }
+
+   else 
+   {
+      box8.style.display="none";
+
+
+
+
+   }
+
+})
+
+
+
+document.getElementById("rb10").addEventListener("click", function(){
+
+
+   if(box9.style.display=="none")
+   {  var n = 0;
+      while(n < 16)
+      {  
+         box[n].style.display = "none";
+         n++;
+      }
+      box9.style.display="block";
+   }
+
+   else 
+   {
+      box9.style.display="none";
+
+
+
+
+   }
+
+})
+
+
+
+document.getElementById("rb11").addEventListener("click", function(){
+
+
+   if(box10.style.display=="none")
+   {  var n = 0;
+      while(n < 16)
+      {  
+         box[n].style.display = "none";
+         n++;
+      }
+      box10.style.display="block";
+   }
+
+   else 
+   {
+      box10.style.display="none";
+
+
+
+
+   }
+
+})
+
+
+
+document.getElementById("rb12").addEventListener("click", function(){
+
+
+   if(box11.style.display=="none")
+   {  var n = 0;
+      while(n < 16)
+      {  
+         box[n].style.display = "none";
+         n++;
+      }
+      box11.style.display="block";
+   }
+
+   else 
+   {
+      box11.style.display="none";
+
+
+
+
+   }
+
+})
+
+
+document.getElementById("link_high_achievers").addEventListener("click", function(){
+
+
+   if(box13.style.display=="none")
+   {  var n = 0;
+      while(n < 16)
+      {  
+         box[n].style.display = "none";
+         n++;
+      }
+      box13.style.display="block";
+   }
+
+   else 
+   {
+      box13.style.display="none";
+
+
+
+
+   }
+
+})
+
+document.getElementById("link_workshop").addEventListener("click", function(){
+
+
+   if(box14.style.display=="none")
+   {  var n = 0;
+      while(n < 16)
+      {  
+         box[n].style.display = "none";
+         n++;
+      }
+      box14.style.display="block";
+   }
+
+   else 
+   {
+      box14.style.display="none";
+
+
+
+
+   }
+
+})
+
+
+
+document.getElementById("link_the_line").addEventListener("click", function(){
+
+
+   if(box15.style.display=="none")
+   {  var n = 0;
+      while(n < 16)
+      {  
+         box[n].style.display = "none";
+         n++;
+      }
+      box15.style.display="block";
+   }
+
+   else 
+   {
+      box15.style.display="none";
+
+
+
+
+   }
+
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// var rightBar = document.getElementsByClassName("r_bar");
+// var showText = [document.getElementById("manger_word"), document.getElementById("about_unit"),document.getElementById("implementation_mechanisms"),document.getElementById("organizational_structure"),document.getElementById("unit_members"),document.getElementById("alumni_stats"),document.getElementById("alumni_honor_board"),document.getElementById("alumni_magazine"),document.getElementById("important_topics") , document.getElementById("unit_models") ,  document.getElementById("photo_album")];
+
+// for (i=0 ; i< rightBar.length; i++)
+// {
+//    rightBar[i].addEventListener("click" , selectElement);
+// }
+
+// function selectElement()
+// {
+
+//    if(showText.style.display="none")
+//       showText.style.display="block";
+//    else
+//       showText.style.display="none";
+   
+
+
+
+
+
+
+
+
+
+
+
+
+// var content = document.getElementById("manger_word");
+// var btn = document.getElementById("rb1");
+
+// btn.addEventListener("click", sele)
+
+
+// function sele (){
+//    if(content.style.display="none")
+//       content.style.display="block";
+//    else
+//       content.style.display="none";
+      
+
+//
